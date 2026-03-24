df93d7666fb59211f1a8b37f005e4af4c600944f
Zarinoow <petits.minous12@gmail.com>
2026-03-24 19:23:03 +0100

    Amélioration du placement du block et de la hitbox

--- src/main/java/fr/cucubany/cucubanymod/blocks/advanced/AtmBlock.java
+++ src/main/java/fr/cucubany/cucubanymod/blocks/advanced/AtmBlock.java
@@ -1,16 +1,35 @@
 package fr.cucubany.cucubanymod.blocks.advanced;
 
 import fr.cucubany.cucubanymod.blocks.DoubleOrientableBlock;
+import fr.cucubany.cucubanymod.blocks.DoubleOrientableBlockHelper;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 import static fr.cucubany.cucubanymod.blocks.DoubleOrientableBlockHelper.HALF;
 
 public class AtmBlock extends DoubleOrientableBlock implements EntityBlock {
+
+    /**
+     * Index :
+     * 0 -> NORTH
+     * 1 -> SOUTH
+     * 2 -> WEST
+     * 3 -> EAST
+     */
+    private static final VoxelShape[] SHAPES = {
+            Block.box(3,   0, 7, 13, 26, 16), // NORTH
+            Block.box(3,   0, 0, 13, 26,  9), // SOUTH
+            Block.box(7,   0, 3, 16, 26, 13), // WEST
+            Block.box(0,   0, 3,  9, 26, 13) // EAST
+    };
 
     public AtmBlock(Properties pProperties) {
         super(pProperties);
@@ -18,9 +37,13 @@ public class AtmBlock extends DoubleOrientableBlock implements EntityBlock {
 
     @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        // On ne crée une BlockEntity que pour la partie basse (celle qui est rendue).
         return state.getValue(HALF) == DoubleBlockHalf.LOWER
                 ? new AtmBlockEntity(pos, state)
                 : null;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return DoubleOrientableBlockHelper.getShape(SHAPES, state);
     }
 }
--- src/main/java/fr/cucubany/cucubanymod/blocks/advanced/AtmBlockEntity.java
+++ src/main/java/fr/cucubany/cucubanymod/blocks/advanced/AtmBlockEntity.java
@@ -5,9 +5,11 @@ import fr.cucubany.cucubanymod.blocks.DoubleOrientableBlockHelper;
 import fr.cucubany.cucubanymod.blocks.HugeBlockAABBHelper;
 import fr.cucubany.cucubanymod.blocks.IBBBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
 public class AtmBlockEntity extends BlockEntity implements IBBBlockEntity {
 
@@ -23,5 +25,20 @@ public class AtmBlockEntity extends BlockEntity implements IBBBlockEntity {
     @Override
     public boolean isMainPart() {
         return DoubleOrientableBlockHelper.isMainPart(getBlockState());
+    }
+
+    @Override
+    public BBRenderType getBBRenderType() {
+        return BBRenderType.BB_CUTOUT;
+    }
+
+    /**
+     * Décale le modèle de 3 pixels vers l'arrière (contre le mur), selon l'orientation.
+     */
+    @Override
+    public Vec3 getModelOffset() {
+        Direction facing = getBlockState().getValue(DoubleOrientableBlockHelper.FACING);
+        double pixels = 0.17375; // 2.75 / 16.0 (= 2.75 pixel en arrière)
+        return new Vec3(-facing.getStepX() * pixels, 0, -facing.getStepZ() * pixels);
     }
 }
