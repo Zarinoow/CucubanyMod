0b4dce68008a1fd9e9ad2944cd881aa70e4aa0e0
Zarinoow <petits.minous12@gmail.com>
2026-04-02 22:31:42 +0200

    Sneak hitbox tweaks (unfinished)

--- src/main/java/fr/cucubany/cucubanymod/hitbox/poses/SneakPoseHandler.java
+++ src/main/java/fr/cucubany/cucubanymod/hitbox/poses/SneakPoseHandler.java
@@ -18,27 +18,33 @@ public class SneakPoseHandler implements IPartPoseHandler {
         double bx = Math.sin(rad); // Vecteur Arrière
         double bz = -Math.cos(rad);
 
-        double headY = 0.95;
+        double headY = 1.0;
         double torsoY = 0.45;
         double armY = 0.35;
-        double legY = 0.0;
-        double sneakBack = 0.2;
+        double sneakBack = 0.25;
+
+        // Configuration Pieds/Jambes
+        float footHeight = 0.15F;           // Hauteur du pied
+        float legHeight = 0.65F - footHeight; // Reste de la jambe
+
+        double footY = -0.15;                // Le pied est dans le sol
+        double legY = 0;           // La jambe commence au-dessus du pied
 
         return switch (part) {
             case HEAD -> new PartTransform[]{ PartTransform.of(x - (bx * 0.1), y + headY, z - (bz * 0.1), 0.5F, 0.5F) };
 
             case TORSO -> {
-                PartTransform merged = PartTransform.of(x + (bx * 0.15), y + torsoY, z + (bz * 0.15), 0.5F, 0.5F);
+                PartTransform merged = PartTransform.of(x + (bx * 0.2), y + torsoY, z + (bz * 0.2), 0.5F, 0.625F);
                 yield new PartTransform[]{ merged, merged };
             }
 
             case ARM_LEFT -> new PartTransform[]{ PartTransform.of(x + (lx * 0.375) + (bx * 0.1), y + armY, z + (lz * 0.375) + (bz * 0.1), 0.25F, 0.60F) };
             case ARM_RIGHT -> new PartTransform[]{ PartTransform.of(x - (lx * 0.375) + (bx * 0.1), y + armY, z - (lz * 0.375) + (bz * 0.1), 0.25F, 0.60F) };
 
-            case LEG_LEFT -> new PartTransform[]{ PartTransform.of(x + (lx * 0.125) + (bx * sneakBack), y + legY, z + (lz * 0.125) + (bz * sneakBack), 0.25F, 0.75F) };
-            case LEG_RIGHT -> new PartTransform[]{ PartTransform.of(x - (lx * 0.125) + (bx * sneakBack), y + legY, z - (lz * 0.125) + (bz * sneakBack), 0.25F, 0.75F) };
-            case FOOT_LEFT -> new PartTransform[]{ PartTransform.of(x + (lx * 0.125) + (bx * sneakBack), y + 0.0, z + (lz * 0.125) + (bz * sneakBack), 0.25F, 0.15F) };
-            case FOOT_RIGHT -> new PartTransform[]{ PartTransform.of(x - (lx * 0.125) + (bx * sneakBack), y + 0.0, z - (lz * 0.125) + (bz * sneakBack), 0.25F, 0.15F) };
+            case LEG_LEFT -> new PartTransform[]{ PartTransform.of(x + (lx * 0.125) + (bx * sneakBack), y + legY, z + (lz * 0.125) + (bz * sneakBack), 0.25F, legHeight) };
+            case LEG_RIGHT -> new PartTransform[]{ PartTransform.of(x - (lx * 0.125) + (bx * sneakBack), y + legY, z - (lz * 0.125) + (bz * sneakBack), 0.25F, legHeight) };
+            case FOOT_LEFT -> new PartTransform[]{ PartTransform.of(x + (lx * 0.125) + (bx * sneakBack), y + footY, z + (lz * 0.125) + (bz * sneakBack), 0.25F, footHeight) };
+            case FOOT_RIGHT -> new PartTransform[]{ PartTransform.of(x - (lx * 0.125) + (bx * sneakBack), y + footY, z - (lz * 0.125) + (bz * sneakBack), 0.25F, footHeight) };
         };
     }
 }
